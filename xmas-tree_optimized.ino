12197f7820de323cef1d61b3830296e590701408
Nicolas <kaldoran@users.noreply.github.com>
2022-11-18 21:41:46 +0100

    Code optimization / Shrinking / Color choice
    Change int to uint8_t when possible.
    Add option of color on function
    Optimize code and make it more modular

--- xmas-tree_optimized.ino
+++ xmas-tree_optimized.ino
@@ -1,53 +1,22 @@
-/*
-   Christmas tree by designer2k2.at
-   Stephan Martin 2016
-   http://www.designer2k2.at
-   https://github.com/designer2k2/xmas-tree
-
-   This code is made to run on a Digispark
-   Feeding Neopixel Rings, stacked to a tree.
-
-   Brightness is reduced as there is only USB power supply.
-
-
-   Needs Adafruit NeoPixel Lib to work: https://github.com/adafruit/Adafruit_NeoPixel
-   Use the build in Library manager in the Arduino IDE: "Adafruit NeoPixel"
-
-   Use the ATTinyCore set to ATtiny85 (Micronucleus / Digispark) in the Arduino IDE
-   https://github.com/SpenceKonde/ATTinyCore
-
-*/
-
 
 #include <Adafruit_NeoPixel.h>    //needed for the WS2812
 
 #define PIN 1					            //Pin 1 is DATA In on the bottom Ring
-#define BRIGHTNESS 40 			      // brightness reduced (about 180mA max, 100mA average)
+#define BRIGHTNESS 255 			      // brightness reduced (about 180mA max, 100mA average)
+#define SIZEOFARRAY(X) sizeof(X) / sizeof(X[0])
 
+#define TOTAL_LED 61 // Fill by hand
+const uint8_t ringsSize[] = {1, 8, 12, 16, 24};
 
+ 
 //Lookups for the X-M-A-S Letters viewed from above:
-const unsigned int charX[] PROGMEM =
-{
-  4, 27, 54, 61, 58, 35, 16, 10, 31, 56, 60, 39, 22
-};
-const unsigned int charM[] PROGMEM =
-{
-  4, 26, 25, 40, 22, 39, 60, 61, 58, 35, 16, 34, 33, 32, 10
-};
-const unsigned int charA[] PROGMEM =
-{
-  4, 27, 42, 53, 60, 51, 37, 19, 49, 58, 57, 61, 46, 31, 10
-};
-const unsigned int charS[] PROGMEM =
-{
-  22, 38, 37, 36, 35, 34, 47, 57, 61, 53, 41, 26, 27, 28, 29, 30, 10
-};
+const uint8_t charX[] PROGMEM = {4, 27, 54, 61, 58, 35, 16, 10, 31, 56, 60, 39, 22};
+const uint8_t charM[] PROGMEM = {4, 26, 25, 40, 22, 39, 60, 61, 58, 35, 16, 34, 33, 32, 10};
+const uint8_t charA[] PROGMEM = {4, 27, 42, 53, 60, 51, 37, 19, 49, 58, 57, 61, 46, 31, 10};
+const uint8_t charS[] PROGMEM = {22, 38, 37, 36, 35, 34, 47, 57, 61, 53, 41, 26, 27, 28, 29, 30, 10};
 
 //Lookup for the Candle light
-const unsigned int candles[] PROGMEM =
-{
-  15, 10, 48, 45, 36, 19, 59, 29, 5, 43, 41, 39, 24, 3, 61
-};
+const uint8_t candles[] PROGMEM = {15, 10, 48, 45, 36, 19, 59, 29, 5, 43, 41, 39, 24, 3, 61};
 
 
 // Parameter 1 = number of pixels in strip
@@ -57,7 +26,7 @@ const unsigned int candles[] PROGMEM =
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(61, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTAL_LED, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -70,24 +39,24 @@ void setup() {
   strip.setBrightness(BRIGHTNESS); // set brightness
   strip.show(); // Initialize all pixels to 'off'
 
-  //XMAS Letters:
-  xmas();
+  // XMAS Letters:
+  xmas(1000);
   delay(2000);
 
 }
 
 void loop() {
 
-  //Tree light:
-  tree();
+  //Tree light [Green]
+  tree(strip.Color(0, 50, 0));
   delay(1000);
 
   //Color crazy:
   colorcrazy();
-  //delay(1000);
+  delay(1000);
 
-  warpdrive();
-  comet();
+  warpdrive(strip.Color(255, 255, 255));
+  comet(strip.Color(0, 0, 255));
 
 
   /*
@@ -99,7 +68,6 @@ void loop() {
     theaterChase(strip.Color(127, 127, 127), 50); // White
     theaterChase(strip.Color(127,   0,   0), 50); // Red
     theaterChase(strip.Color(  0,   0, 127), 50); // Blue
-
     rainbow(20);
     rainbowCycle(20);
     theaterChaseRainbow(50);
@@ -109,113 +77,53 @@ void loop() {
 //Sub-----------------------------------------------------------------------
 
 //Comet
-void comet() {
+void comet(uint32_t _color) {
   for (uint16_t i = strip.numPixels(); i > 0; i--) {
-    strip.setPixelColor(i, strip.Color(0, 0, 255));
+    strip.setPixelColor(i, _color);
     fadethemall(10);
     fadethemall(10);
   }
 }
 
 //From top down white pulses
-void warpdrive() {
+void warpdrive(uint32_t _color) {
+  uint8_t totalLed = TOTAL_LED;
+  uint8_t ringSize, led;
 
-  //Top Led
-  strip.setPixelColor(60, strip.Color(255, 255, 255));
-  strip.show();
-  //fade a bit
-  for (int i = 0; i < 20; i++)
-  {
-    fadethemall(20);
-  }
-  //8 Ring
-  for (int i = 52; i < 60; i++)
-  {
-    strip.setPixelColor(i, strip.Color(255, 255, 255));
-  }
-  strip.show();
-  //fade a bit
-  for (int i = 0; i < 20; i++)
-  {
-    fadethemall(20);
-  }
-  //12 Ring
-  for (int i = 40; i < 52; i++)
-  {
-    strip.setPixelColor(i, strip.Color(255, 255, 255));
-  }
-  strip.show();
-  //fade a bit
-  for (int i = 0; i < 20; i++)
-  {
-    fadethemall(20);
-  }
-  //16 Ring
-  for (int i = 24; i < 40; i++)
-  {
-    strip.setPixelColor(i, strip.Color(255, 255, 255));
-  }
-  strip.show();
-  //fade a bit
-  for (int i = 0; i < 20; i++)
-  {
-    fadethemall(20);
-  }
-  //24 Ring
-  for (int i = 0; i < 24; i++)
-  {
-    strip.setPixelColor(i, strip.Color(255, 255, 255));
-  }
-  strip.show();
-  //fade a bit
-  for (int i = 0; i < 20; i++)
-  {
-    fadethemall(20);
+
+  for (count = 0;count < SIZEOFARRAY(ringsSize); ++count) {
+    // 60 -> 61 (1) | 52 -> 60 (8) | 40 -> 52 (12) ...
+    for (led = totalLed - ringsSize[count]; led < totalLed; ++led) {
+        strip.setPixelColor(led, _color);
+    }
+    strip.show();
+
+    for (uint8_t i = 0; i < 20; i++) fadethemall(20);
+    totalLed -= ringsSize[count];
   }
 }
 
 //This reduces the brightness of all leds
-void fadethemall(uint8_t wait) {
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
-    uint32_t color = strip.getPixelColor(i);
-    int r;
-    int g;
-    int b;
+void fadethemall(uint8_t _wait) {
+  uint8_t r, g, b, counter;
+  uint32_t color;  
+
+  for (counter = 0; counter < TOTAL_LED; counter++) {
+    color = strip.getPixelColor(i);
+
     r = (uint8_t)(color >> 16);
     g = (uint8_t)(color >>  8);
     b = (uint8_t)color;
 
-    if (r > 0)
-    {
-      r = r - 1;
-    }
-    else
-    {
-      r = 0;
-    }
-
-    if (g > 0)
-    {
-      g = g - 1;
-    }
-    else
-    {
-      g = 0;
-    }
-
-    if (b > 0)
-    {
-      b = b - 1;
-    }
-    else
-    {
-      b = 0;
-    }
+    if (r > 0) --r;
+    if (g > 0) --g;
+    if (b > 0) --b;
 
     strip.setPixelColor(i, strip.Color(r, g, b));
   }
+
   strip.show();
-  delay(wait);
+  delay(_wait);
 }
 
 //This drives the WS2812 in a crazy pattern, fun!
@@ -227,14 +135,13 @@ void colorcrazy() {
 }
 
 //This lights up the tree in green, then add the white "candles"
-void tree() {
+void tree(uint32_t _color) {
 
-  colorWipe(strip.Color(0, 50, 0), 50); // Green
+  colorWipe(_color, 50); // Green
 
   //light "candles"
   //Show the S:
-  for (int i = 0; i < 16; i++)
-  {
+  for (uint8_t i = 0; i < SIZEOFARRAY(candles); i++) {
     strip.setPixelColor(pgm_read_word(&candles[i]) - 1, strip.Color(255, 255, 255));
     strip.show();
     delay(50);
@@ -243,65 +150,63 @@ void tree() {
 }
 
 //This shows the X-M-A-S when viewed from above
-void xmas() {
-
+void xmas(uint16_t _wait) {
+  uint8_t i;
   colorWipe(strip.Color(0, 0, 0), 0); // blank
 
-
-  //The size of the PROGMEM is hardcoded, as the sizeof does return crap on the ATtiny.
-
   //Show the X:
-  for (int i = 0; i < 13; i++)
+  for (i = 0; i < SIZEOFARRAY(charX); i++)
   {
-    strip.setPixelColor(pgm_read_word(&charX[i]) - 1, strip.Color(255, 0, 0));
+    strip.setPixelColor(pgm_read_byte(&charX[i]) - 1, strip.Color(255, 0, 0));
     strip.show();
     delay(50);
   }
-  delay(1000);
+  delay(_wait);
   colorWipe(strip.Color(0, 0, 0), 0); // blank
 
   //Show the M:
-  for (int i = 0; i < 15; i++)
+  for (i = 0; i < SIZEOFARRAY(charM); i++)
   {
-    strip.setPixelColor(pgm_read_word(&charM[i]) - 1, strip.Color(0, 255, 0));
+    strip.setPixelColor(pgm_read_byte(&charM[i]) - 1, strip.Color(0, 255, 0));
     strip.show();
     delay(50);
   }
-  delay(1000);
+  delay(_wait);
   colorWipe(strip.Color(0, 0, 0), 0); // blank
 
   //Show the A:
-  for (int i = 0; i < 15; i++)
+  for (i = 0; i < SIZEOFARRAY(charA); i++)
   {
-    strip.setPixelColor(pgm_read_word(&charA[i]) - 1, strip.Color(0, 0, 255));
+    strip.setPixelColor(pgm_read_byte(&charA[i]) - 1, strip.Color(0, 0, 255));
     strip.show();
     delay(50);
   }
-  delay(1000);
+  delay(_wait);
   colorWipe(strip.Color(0, 0, 0), 0); // blank
 
   //Show the S:
-  for (int i = 0; i < 17; i++)
+  for (i = 0; i < SIZEOFARRAY(charS); i++)
   {
-    strip.setPixelColor(pgm_read_word(&charS[i]) - 1, strip.Color(125, 125, 125));
+    strip.setPixelColor(pgm_read_byte(&charS[i]) - 1, strip.Color(125, 125, 125));
     strip.show();
     delay(50);
   }
-  delay(1000);
+
+  delay(_wait);
   colorWipe(strip.Color(0, 0, 0), 0); // blank
 
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
+void colorWipe(uint32_t _c, uint8_t _wait) {
+  for (uint8_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, _c);
     strip.show();
-    delay(wait);
+    delay(_wait);
   }
 }
 
-void rainbow(uint8_t wait) {
+void rainbow(uint8_t _wait) {
   uint16_t i, j;
 
   for (j = 0; j < 256; j++) {
@@ -309,12 +214,12 @@ void rainbow(uint8_t wait) {
       strip.setPixelColor(i, Wheel((i + j) & 255));
     }
     strip.show();
-    delay(wait);
+    delay(_wait);
   }
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
+void rainbowCycle(uint8_t _wait) {
   uint16_t i, j;
 
   for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
@@ -322,20 +227,20 @@ void rainbowCycle(uint8_t wait) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
-    delay(wait);
+    delay(_wait);
   }
 }
 
 //Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
-    for (int q = 0; q < 3; q++) {
+void theaterChase(uint32_t _c, uint8_t _wait) {
+  for (uint8_t j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (uint8_t q = 0; q < 3; q++) {
       for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
-        strip.setPixelColor(i + q, c);  //turn every third pixel on
+        strip.setPixelColor(i + q, _c);  //turn every third pixel on
       }
       strip.show();
 
-      delay(wait);
+      delay(_wait);
 
       for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
         strip.setPixelColor(i + q, 0);      //turn every third pixel off
@@ -345,17 +250,17 @@ void theaterChase(uint32_t c, uint8_t wait) {
 }
 
 //Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
+void theaterChaseRainbow(uint8_t _wait) {
   for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
-    for (int q = 0; q < 3; q++) {
-      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+    for (uint8_t q = 0; q < 3; q++) {
+      for (uint8_t i = 0; i < strip.numPixels(); i = i + 3) {
         strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
       }
       strip.show();
 
-      delay(wait);
+      delay(_wait);
 
-      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+      for (uint8_t i = 0; i < strip.numPixels(); i = i + 3) {
         strip.setPixelColor(i + q, 0);      //turn every third pixel off
       }
     }
